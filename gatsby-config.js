f62ec34eec024299e1bedcc63c96d4417e6fa909
Juan Víctor Artola Romero <48799012+JViktoRArtola@users.noreply.github.com>
2021-01-21 16:46:20 -0500

    Configure gatsby-source-datocms to connect to DatoCMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-datocms',
+      options: {
+        apiToken: '2b5c36caec3e03a36609afd57f741c'
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
