a8ab55c7cfab78fba2dad09b54cc010f797a5b75
Juan Víctor Artola Romero <48799012+JViktoRArtola@users.noreply.github.com>
2021-01-22 13:31:51 -0500

    Use roomPreview component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,39 @@ import React from "react"
 import Layout from "../components/layout"
 import HotelImage from "../components/image"
 import ContentHome from "../components/contentHome"
+import useBedRooms from "../hooks/useBedRooms"
+import RoomPreview from "../components/roomPreview"
+import styled from "@emotion/styled"
 
-const IndexPage = () => (
-  <Layout>
-    <HotelImage />
-    <ContentHome />
-  </Layout>
-)
+
+const RoomList = styled.ul`
+  max-width: 1200px;
+  width: 95%;
+  margin: 4rem auto 0 auto;
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    column-gap: 3rem
+  }
+`
+
+const IndexPage = () => {
+  const rooms = useBedRooms()
+  return (
+    <Layout>
+      <HotelImage/>
+      <ContentHome/>
+
+      <RoomList>
+        {rooms.map(room => (
+          <RoomPreview
+            key={room.id}
+            room={room}
+          />
+        ))}
+      </RoomList>
+    </Layout>
+  )
+}
 
 export default IndexPage
